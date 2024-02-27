cd4693898a36221663f25916d92681d4e1e4b252
André Souza Galvão <161396292+andregalvao2030@users.noreply.github.com>
2024-02-27 02:23:45 -0300

    Add files via upload

---
+++ Dimension1.java
@@ -0,0 +1,14 @@
+public class Dimension1 {
+    public static void main(String[] args) {
+        System.out.println("Running Dimensional Equation....");
+        double eq1 = 2.18;
+        double eq2 = 3.14;
+        double eq3 = 6.66;
+
+        double equation = (-eq2 + ((eq2*eq2) - 4*eq1*eq3))/2*eq1;;
+
+        System.out.println("The Dimensional Coeficient is:" + equation);
+
+
+    }
+}
